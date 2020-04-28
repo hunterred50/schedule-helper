757ec5c4979b41092959bc44a8dd3df68fff201d
Hunter Redding <hunterred50@gmail.com>
2020-04-28 10:25:32 -0400

    connected mongodb through mongoose

--- schedule-helper-core/app.js
+++ schedule-helper-core/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+mongoose.connect('mongodb+srv://hunter:riolu1@schedulecluster-ey7zq.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
+app.use('/graphql', graphqlHTTP({
+  schema, // both names are the same so es6 doesn't need schema: schema
+  graphiql: true,
 }));
 
 app.listen(4000, () => {
